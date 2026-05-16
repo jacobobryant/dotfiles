fb78bcc195b1a71cde65aa00f06bbc5a3bbbe61e
Jacob O'Bryant <git@jacobobryant.com>
2026-05-16 22:16:59 +0000

    typo

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@ ln -sf $PWD/gitconfig ~/.gitconfig
 curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz
 sudo rm -rf /opt/nvim-linux-x86_64
 sudo tar -C /opt -xzf nvim-linux-x86_64.tar.gz
-sudo ln -sf /opt/nvim-linux-x86_4/bin/nvim /usr/local/bin
+sudo ln -sf /opt/nvim-linux-x86_64/bin/nvim /usr/local/bin
 
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
